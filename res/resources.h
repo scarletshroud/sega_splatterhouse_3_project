e45320e2749d28b487fe1491c7f326ca4226e4c7
slayer404 <doomer888@protonmail.com>
2021-11-21 14:26:15 +0300

    added title music and background resources

--- res/resources.h
+++ res/resources.h
@@ -1,7 +1,10 @@
 #ifndef _RES_RESOURCES_H_
 #define _RES_RESOURCES_H_
 
+extern const u8 title_track[19456];
+extern const u8 sample_track[186624];
 extern const SpriteDefinition rick;
 extern const SpriteDefinition ball;
+extern const Image title_background;
 
 #endif // _RES_RESOURCES_H_
--- res/resources.res
+++ res/resources.res
@@ -1,2 +1,7 @@
+IMAGE title_background "sprites/welcome_background.png" NONE
+
 SPRITE rick "sprites/rick.png" 4 9 NONE
 SPRITE ball "sprites/ball.png" 1 1 NONE
+
+XGM title_track "music/title_theme.vgm" -1
+XGM sample_track "music/sample.vgm" -1
