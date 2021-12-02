def00c8280ff061edbed706db02a9fda27686d10
slayer404 <doomer888@protonmail.com>
2021-12-03 01:24:29 +0300

    basic control in few states

--- src/control.c
+++ src/control.c
@@ -1,19 +1,65 @@
-#include "headers/control.h"
+#include "control.h"
 
 #include <genesis.h>
 
-void titleControlHandler(u16 joy, u16 changed, u16 state) {
+#include "game.h"
+#include "player.h"
+#include "menu_state.h"
+#include "play_state.h"
 
+void title_control_handler(unsigned short joy, unsigned short changed, unsigned short state) {
+    if (joy == JOY_1) {
+        if (changed & state & BUTTON_START) {
+            set_game_state(MENU_STATE);
+        }
+    }
 }
 
-void playControlHandler(u16 joy, u16 changed, u16 state) {
+void play_control_handler(unsigned short joy, unsigned short changed, unsigned short state) {
+    if (joy == JOY_1) {
+        if (changed & state & BUTTON_LEFT) {
+           player_move_left(p);
+        }
 
+        if (changed & state & BUTTON_RIGHT) {
+           player_move_right(p);
+        }
+
+        if (changed & state & BUTTON_UP) {
+           player_move_up(p);
+        }
+
+        if (changed & state & BUTTON_DOWN) {
+           player_move_down(p);
+        }
+    }
 }
 
-void menuControlHandler(u16 joy, u16 changed, u16 state) {
+void menu_control_handler(unsigned short joy, unsigned short changed, unsigned short state) {
+    if (joy == JOY_1) {
 
+        if (changed & state & BUTTON_UP) {
+            move_cursor_up(); 
+        }
+
+        if (changed & state & BUTTON_DOWN) {
+            move_cursor_down(); 
+        }
+
+        if (changed & state & BUTTON_START) {
+            switch (current_menu_option) { 
+                case MENU_OPTION_START:
+                    set_game_state(PLAY_STATE);
+                    break;
+                case MENU_OPTION_OPTIONS:
+                    break;
+                case MENU_OPTION_EXIT:
+                    break;
+            }        
+        }
+    }
 }
 
-void optionsControlHandler(u16 joy, u16 changed, u16 state) {
+void options_control_handler(unsigned short joy, unsigned short changed, unsigned short state) {
 
 }
---
+++ src/control.h
@@ -0,0 +1,11 @@
+#ifndef _CONTROL_H_
+#define _CONTROL_H_
+
+#include <genesis.h>
+
+void title_control_handler(unsigned short joy, unsigned short changed, unsigned short state);
+void play_control_handler(unsigned short joy, unsigned short changed, unsigned short state); 
+void menu_control_handler(unsigned short joy, unsigned short changed, unsigned short state);
+void options_control_handler(unsigned short joy, unsigned short changed, unsigned short state); 
+
+#endif
