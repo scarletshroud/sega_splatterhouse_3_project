6b775c8b3d697f0bb9ff3e5e5c663362d358fda4
slayer404 <doomer888@protonmail.com>
2022-02-02 18:19:56 +0300

    container for zombies

--- src/list.c
+++ src/list.c
@@ -1,12 +1,23 @@
 #include "list.h"
 
+static struct zombie_list* head; 
+static struct zombie_list* current;
+
+struct zombie_list get_zombie_list_head() {
+    return *head;
+}
+
+struct zombie_list get_zombie_list_current() {
+    return *current;
+} 
+
 bool zombie_list_empty() {
     return head == NULL; 
 }
 
 uint8_t zombie_list_size() {
     uint8_t size = 0; 
-    struct zombie_list *current = head; 
+    struct zombie_list *current = head;
 
     for (; current != NULL; current = current->next) {
         size++; 
@@ -25,14 +36,32 @@ void zombie_list_clear() {
         free(prev->z);
         free(prev); 
     }
-} 
+}
 
+void zombie_list_remove(struct zombie_list* head, struct zombie_list* z) {
+    struct zombie_list* current = head;
+    struct zombie_list* prev = NULL; 
+
+    while (current != NULL) {
+        if (current == z) {
+            KLog_U1("yep: ", 2);
+            if (prev != NULL) {
+                prev->next = current->next;
+            }
+            free(current); 
+        }
+        prev = current; 
+        current = current->next; 
+    }    
+}
 
 void zombie_list_pop_back() {
     
-
 }
 
-void zombie_list_push_back(struct zombie *z) {
-    
+void zombie_list_push_back(struct zombie_list** head, const struct zombie* const z) {
+    struct zombie_list* node = (struct zombie_list*)malloc(sizeof(struct zombie_list));
+    node->z = z; 
+    node->next = (*head);
+    (*head) = node; 
 } 
--- src/list.h
+++ src/list.h
@@ -3,20 +3,21 @@
 
 #include "zombie.h"
 
-    struct zombie_list {
-        struct zombie *z;
-        struct node *next;          
-    };
+struct zombie_list {
+    struct zombie* z;
+    struct node* next;          
+};
 
-    struct zombie_list *head; 
-    struct zombie_list *current; 
+struct zombie_list get_zombie_list_head();
+struct zombie_list get_zombie_list_current();  
 
-    bool zombie_list_empty();
-    uint8_t zombie_list_size();
+void zombie_list_pop_back();
+void zombie_list_push_back(struct zombie_list** head, const struct zombie* const z); 
+ 
+void zombie_list_clear();
+void zombie_list_remove(struct zombie_list* head, struct zombie_list* z); 
 
-    void zombie_list_clear(); 
-    void zombie_list_pop_back();
-
-    void zombie_list_push_back(struct zombie *z); 
+bool zombie_list_empty();
+uint8_t zombie_list_size();
 
 #endif
